7ba30e786dbc88b6a7232e38d677668b5ca91f23
CyberButton <87766623307Kk>
2023-07-10 12:53:23 +0600

    quiz componenet

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function Quiz() {
-    return(<>Almas Balabas</>)
+    /**prev button event handler */
+    function onPrev() {
+        console.log("on prev clicked")
+    }
+
+    /**next button event handler */
+    function onNext() {
+        console.log("on next clicked")
+    }
+
+    return(
+        <div className="container">
+            <h1 className="title text-light">Quiz Application</h1>
+
+            {/* display questions */}
+
+            <div className="grid">
+                <button className="btn prev" onClick={onPrev}>Prev</button>
+                <button className="btn next" onClick={onNext}>Next</button>
+            </div>
+
+        </div>
+    )
 }
